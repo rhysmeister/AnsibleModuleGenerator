49e7b7ac2057e239653cef45a1d453c4b9bedcca
Rhys Campbell <rhys.james.campbell@googlemail.com>
2019-05-05 20:46:13 +0200

    Move import

--- cassandra_module_type1.py
+++ cassandra_module_type1.py
@@ -9,9 +9,9 @@ cog.outl("# %s" % header['github_url'])
 [[[end]]]
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
 from ansible.module_utils.basic import AnsibleModule, load_platform_subclass
 import socket
-from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 [[[cog
